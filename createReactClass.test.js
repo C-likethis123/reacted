19b40042a7405b09b04324aa61c372bde5575551
Chow Jia Ying <chowjiaying211@gmail.com>
2022-07-21 21:54:57 +0800

    Failing test: Implement component changes with props

--- createReactClass.test.js
+++ createReactClass.test.js
@@ -8,5 +8,21 @@ test('it renders a plain HTML string to the page', () => {
   const paragraphComponent = Paragraph();
   expect(paragraphComponent).toBe(`<p>Hello, World!</p>`);
 });
+
+test('component changes according to props', () => {
+  const Title = createReactClass({
+    render: function() {
+      return "<h1>{props.name}</h1>";
+    }
+  });
+
+  const firstTitle = Title({
+    name: "Hello, World!"
+  });
+  const secondTitle = Title({
+    name: "Different Title"
+  });
+  expect(firstTitle).toBe("<h1>Hello, World!</h1>");
+  expect(secondTitle).toBe("<h1>Different Title</h1>");
 });
 
