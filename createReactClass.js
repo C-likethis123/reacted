bc50724f635ad1d85ba3b56ff6b7bd4aba27c02b
Chow Jia Ying <chowjiaying211@gmail.com>
2022-07-21 21:55:25 +0800

    Passing test: Implement component changes with props

--- createReactClass.js
+++ createReactClass.js
@@ -1,5 +1,15 @@
 const createReactClass = function(methods) {
-  return methods.render;
+  return (props) => {
+    if (!props) {
+      props = {}
+    }
+
+    let componentTemplate = methods.render();
+    for (const prop in props) {
+      componentTemplate = componentTemplate.replace(`{props.${prop}}`, props[prop]);
+    }
+    return componentTemplate;
+  }
 }
 
 module.exports = createReactClass;
