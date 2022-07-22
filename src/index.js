fef632b57f898e82a72a0d3d46a25ec50a9b6a0e
Chow Jia Ying <chowjiaying211@gmail.com>
2022-07-22 21:36:30 +0800

    Modify index.js and index.html structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import React from '../dist/react';
+import ReactDOM from '../dist/domRender';
+
 // Get an element by id
 const domRoot = document.getElementById("root");
 
+const Test = (props) => <div>{props.title}</div>;
+
 const element = <div id="container">
   <input type="text" value="foo" />
+  <Test title="Hello World"></Test>
   <a href="/bar" />
   <span></span>
 </div>;
