fb7cb8a3fcea2ac477de4ff46fafd11c909bc8cd
Chow Jia Ying <chowjiaying211@gmail.com>
2022-07-22 22:08:56 +0800

    Refactor jest tests

--- tests/domRender.test.js
+++ tests/domRender.test.js
@@ -2,9 +2,24 @@
  * @jest-environment jsdom
  */
 import ReactDOM from '../lib/domRender';
-test('it renders textnodes', () => {
-  const rootNode = document.createElement("div");
+
+let rootNode;
+
+beforeEach(() =>{
+  rootNode = document.createElement("div");
   rootNode.id = "root";
+});
+
+test('it renders textnodes', () => {
   ReactDOM.render("Hello World!", rootNode);
   expect(rootNode.outerHTML).toBe(`<div id="root">Hello World!</div>`);
+});
+
+test('it renders elements from virtual DOM', () => {
+  const vElem = {
+    type: "div",
+    children: "Hello",
+  };
+  ReactDOM.render(vElem, rootNode);
+  expect(rootNode.outerHTML).toBe(`<div id="root"><div>Hello</div></div>`);
 });
